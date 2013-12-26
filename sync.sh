e94051777e4c35ed33cc26ffa1f23c4991d8f0bf
Michael Ball <cycomachead@gmail.com>
2013-12-25 23:24:09 -0800

    tweaks and disabling reversing for now

--- sync.sh
+++ sync.sh
@@ -12,17 +12,22 @@ remote_prefix="/Volumes/Michael-1/Users/Michael/"
 excls="iPod Photo Cache"
 
 source_dir="$local_prefix$ap_loc"
-dest_dir="$remote_prefix$ap_loc"
+dest_dir="/Volumes/Michael-1/$ap_loc"
+# dest_dir="$remote_prefix$ap_loc"
 
-command="rsync -ahvzP --itemize-changes --progress --delete --exclude=\"$excls\""
-rev="$1"
-if [rev -eq ""]; then
-    echo "Source Directory: $source_dir"
-    echo "Destination Directory: $dest_dir"
-    $command "$source_dir" "$dest_dir"
-else
-    echo "Source Directory: $dest_dir"
-    echo "Destination Directory: $source_dir"
-    $command "$dest_dir" "$source_dir"
-fi
+comm="rsync -ahvzP --itemize-changes --progress --delete --exclude=\"$excls\""
+
+echo "Source Directory: $source_dir"
+echo "Destination Directory: $dest_dir"
+$comm "$source_dir" "$dest_dir"
+# rev="$1"
+# if [$rev -eq ""]; then
+#     echo "Source Directory: $source_dir"
+#     echo "Destination Directory: $dest_dir"
+#     $comm "$source_dir" "$dest_dir"
+# else
+#     echo "Source Directory: $dest_dir"
+#     echo "Destination Directory: $source_dir"
+#     $comm "$dest_dir" "$source_dir"
+# fi
 
